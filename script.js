4cffbb79c7e754d3cd4d040b6f3b177a79912868
Nikita Zelenkov <zelenkov4462@gmail.com>
2022-12-15 08:40:37 +0700

    init

---
+++ script.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        type:  'bullets',
+        clickable: true
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+    slidesPerGroup: 1,
+    slideToClickedSlide: true,
+    centeredSlides: true
+})
+
+const slider = document.querySelector('.swiper-container').swiper;
+
